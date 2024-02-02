5d4e47332a394a92bed6caa00308bf0474b15e9b
yuvanandini <dharmadanandini@gmail.com>
2024-02-02 14:59:06 +0530

    mail id changed

--- contactMail.php
+++ contactMail.php
@@ -21,14 +21,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'rameshpilli1428@gmail.com'; // Your Gmail email address
-        $mail->Password = 'jjpksiywaevdyyrc'; // Your Gmail password
+        $mail->Username = 'mail.mythrienterprises@gmail.com'; // Your Gmail email address
+        $mail->Password = 'aeyhkwhguxuqsltn'; // Your Gmail password
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
         // Recipients
-        $mail->setFrom('rameshpilli1428@gmail.com', 'mythri'); // Your Gmail email and name
-        $mail->addAddress('rameshpilli1428@gmail.com', 'mythri'); // Recipient's email and name
+        $mail->setFrom('mail.mythrienterprises@gmail.com', 'mythri'); // Your Gmail email and name
+        $mail->addAddress('mail.mythrienterprises@gmail.com', 'mythri'); // Recipient's email and name
 
         // Content
         $mail->isHTML(true);
